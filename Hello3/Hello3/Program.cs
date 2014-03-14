f414077c50704c721532b0462fbb0adc4e11d3c7
fodrh1201 <fodrh1201@gmail.com>
2014-03-14 14:40:11 +0900

    last

--- Hello3/Hello3/Program.cs
+++ Hello3/Hello3/Program.cs
@@ -7,6 +7,10 @@ namespace helloworld
 		public static void Main (string[] args)
 		{
 			Console.WriteLine("안녕하세요?");
+			Console.WriteLine("bye");
+			Console.WriteLine("Hello again.");
+			Console.WriteLine("bye again!");
+
 		}
 	}
 }
